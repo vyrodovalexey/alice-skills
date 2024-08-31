d932867e65a4175a21db55d68cd52f9131626ac7
alexey <vyrodov.alexey@gmail.com>
2024-08-31 23:38:43 +0300

    test

---
+++ cmd/skill/main.go
@@ -0,0 +1,39 @@
+// пакеты исполняемых приложений должны называться main
+package main
+
+import (
+	"net/http"
+)
+
+// функция main вызывается автоматически при запуске приложения
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+// функция run будет полезна при инициализации зависимостей сервера перед запуском
+func run() error {
+	return http.ListenAndServe(`:8080`, http.HandlerFunc(webhook))
+}
+
+// функция webhook — обработчик HTTP-запроса
+func webhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		// разрешаем только POST-запросы
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// установим правильный заголовок для типа данных
+	w.Header().Set("Content-Type", "application/json")
+	// пока установим ответ-заглушку, без проверки ошибок
+	_, _ = w.Write([]byte(`
+      {
+        "response": {
+          "text": "Извините, я пока ничего не умею"
+        },
+        "version": "1.0"
+      }
+    `))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vyrodovalexey/alice-skills
+
+go 1.23.0
